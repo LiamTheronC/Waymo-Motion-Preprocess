35a183a29e7c2ee3b17d2e67707046412571b44a
CAI JIACHENG <52328101+LiamTheronC@users.noreply.github.com>
2023-08-26 15:07:29 +0800

    Update preprocess_exe.py

--- preprocess/preprocess_exe.py
+++ preprocess/preprocess_exe.py
@@ -1,44 +1,61 @@
 import sys
-sys.path.append('/home/avt/prediction/Waymo/working/')
+import os
 import numpy as np
 import torch
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+woring_dir = os.path.dirname(script_dir)
+sys.path.append(woring_dir)
+
 from dataLoader.data_loader import Waymo_Motion_DataLoader
 from preprocess import Waymo_Motion_Preprocess
 
-config = dict()
-config['train'] = '/home/avt/prediction/Waymo/dataset/train'
-config['val'] = '/home/avt/prediction/Waymo/dataset/validation'
-config['pred_range'] = [-100.0, 100.0, -100.0, 100.0]
-config['num_scales'] = 6
-config['cross_dist'] = 6
-config['downsample_factor'] = 10
-config["dim_feats"] = {'xyvp':[6,2], 'xyz':[4,3], 'xy':[3,2], 'xyp':[4,2], 'vp':[4,2], 'vpt':[5,2]}
-config['type_feats'] = 'vp'
-config['f'] = '100f'
-config['aug'] = False
-config['light'] = True
-config['delete'] = ['scenario_id', 'time_stamps', 'current_time_index',
-                    'sdc_index', 'objects_of_interest', 'road_info']
+def parse_args():
+  parser = argparse.ArgumentParser(description='waymo motion dataset preprocess')
+  parser.add_argument('path', choices=['train', 'val', 'test'], help='file path for the preprocess')
+  parser.add_argument('--num-scale', default=6, help='scale of predecessor and successor node pairs')
+  parser.add_argument('--cross-dist', default=6, help='cross distance for node left paris and right pairs')
+  parser.add_argument('--downsample-factor', default=10, help='downsample factor for graph')
+  parser.add_argument('--type-feats', choices=['vp', 'xyvp', 'xyz','xyp'], default='vp', help='types of feature')
+  args = parser.parse_args()
+
+  return args
 
 def main():
 
-    path = config['train']
+  args = parse_args()
+  
+  config = dict()
+  config['path'] = os.path.join(working_dir, 'dataset', args.path)
+  config['pred_range'] = [-100.0, 100.0, -100.0, 100.0]
+  config['num_scales'] = args.num_scale
+  config['cross_dist'] = args.cross_dist
+  config['downsample_factor'] = args.downsample_factor
+  config["dim_feats"] = {'xyvp':[6,2], 'xyz':[4,3], 'xy':[3,2], 'xyp':[4,2], 'vp':[4,2], 'vpt':[5,2]}
+  config['type_feats'] = args.type_feats
+  config['aug'] = False
+  config['light'] = True
+  config['delete'] = ['scenario_id', 'time_stamps', 'current_time_index',
+                      'sdc_index', 'objects_of_interest', 'road_info']
 
-    if path == config['train']:
-        word = 'train'
-    elif path == config['val']:
-        word = 'val'
-    train_dataset = Waymo_Motion_DataLoader(path)
-   
-    for j in range(100):
-        scen_list = train_dataset[j].read_TFRecord
-        processed_list = Waymo_Motion_Preprocess(scen_list, config)
+  out_path = os.path.join(working_dir, 'data_processed', args.path)
+  if not os.path.exists(subdir_path):
+    os.makedirs(subdir_path)
+    
+  train_dataset = Waymo_Motion_DataLoader(path)
+  all_files = os.listdir(working_dir)
+  tfrecord_files = [f for f in all_files if '.tfrecord' in f]
+  num_pt_files = len( tfrecord_files)
+ 
+  for j in range(num_pt_files):
+      scen_list = train_dataset[j].read_TFRecord
+      processed_list = Waymo_Motion_Preprocess(scen_list, config)
 
-        for i,p in enumerate(processed_list):
-            torch.save(p,'/home/avt/prediction/Waymo/data_processed/'+ config['type_feats'] + '/'+ word +'_' + config['f'] + '/'+ str(j) + '_' + str(i)  +'.pt')
-        
-        del processed_list
-        del scen_list
+      for i,p in enumerate(processed_list):
+          torch.save(p, out_path + '/' + str(j) + '_' + str(i)  +'.pt')
+      
+      del processed_list
+      del scen_list
 
 if __name__ == "__main__":
     main()
